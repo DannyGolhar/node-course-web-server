af74c848281adc217d123622fa00523fd99a8fd8
Dnyaneshwar Golhar <dgolhar@shortlist.net>
2018-04-19 14:43:49 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const fs = require('fs');
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', () => {
+   return new Date().getFullYear()
+})
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now} ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (error) => {
+        if(error) {
+            console.log('Unable to print server log');
+        }
+    })
+
+    next();
+})
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+})
+app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase()
+ })
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Welcome</h1>'); res.send({     name: 'Danny',     likes:
+    // ['cricket', 'Tennis', 'hockey'] })
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        name: 'Danny',
+        currentYear: new Date().getFullYear(),
+        likes: ['cricket', 'Tennis', 'hockey']
+    })
+})
+
+app.get('/about', (req, res) => {
+    // res.send('About page');
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+        currentYear: new Date().getFullYear()
+    });
+})
+
+app.get('/bad', (req, res) => {
+    res.send({errorMessage: 'Unable to handle request'})
+})
+
+app.listen(5000);
