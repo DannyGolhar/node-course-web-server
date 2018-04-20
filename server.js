21a174af1f2b2f00c4c8db9bca06d7fa1a3452fa
Dnyaneshwar Golhar <dgolhar@shortlist.net>
2018-04-20 11:52:03 +0530

    Add a project page

--- server.js
+++ server.js
@@ -51,6 +51,14 @@ app.get('/about', (req, res) => {
     });
 })
 
+app.get('/projects', (req, res) => {
+    // res.send('About page');
+    res.render('projects.hbs', {
+        pageTitle: 'Project page',
+        currentYear: new Date().getFullYear()
+    });
+})
+
 app.get('/bad', (req, res) => {
     res.send({errorMessage: 'Unable to handle request'})
 })
